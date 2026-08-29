e9bfae68cf31bda2e8df2876320ac9c356d0be78
Sabarinathan Ramanathan <sabarinathan.code@gmail.com>
2026-08-30 01:59:16 +0530

    refactor: establish application entry point

---
+++ lib/core/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TrailuneApp extends StatelessWidget {
+  const TrailuneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trailune',
+      home: Scaffold(body: Center(child: Text('Trailune'))),
+    );
+  }
+}
